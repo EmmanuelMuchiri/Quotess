83bf499d4a84afe3cf7b3c783fbc2ba13f8b0c95
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-07-07 23:46:14 +0300

    updated my quotes to check whether the pipe is working

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,8 +10,8 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2019,3,14),0,0),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9),0,0)
+    new Quote(1, 'I have no special talent. I am only passionately curious', 'Albert Einstein',new Date(2019,7,7),0,0),
+    new Quote(2,'I do not fear computers. I fear lack of them','Isaac Asimov',new Date(2019,6,9),0,0)
   ];
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
